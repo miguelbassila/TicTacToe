dc0bd39a166915de933abf48a7d00a13139ce512
Miguel Bassila <miguelbassila@me.com>
2016-11-24 22:16:32 -0200

    Add properties for Gameboard struct Props

--- Chapter2/Chapter2/UI/GameBoard/GameBoard.swift
+++ Chapter2/Chapter2/UI/GameBoard/GameBoard.swift
@@ -46,5 +46,30 @@ extension GameBoard {
     var frame: CGRect = .zero
     var key: String?
     var alpha: CGFloat = 1.0
+
+    var isGameFinished: Bool
+    var turn: Player
+    var board: [Player?]
+    var winningLine: [Int]?
+
+    var player1Score: Int
+    var player2Score: Int
+
+    var isLoading: Bool
+
+    init() {
+      self.isGameFinished = true
+      self.turn = .one
+      self.player1Score = 0
+      self.player2Score = 0
+      self.winningLine = nil
+      self.isLoading = false
+
+      self.board = [
+        nil, nil, nil,
+        nil, nil, nil,
+        nil, nil, nil,
+      ]
+    }
   }
 }
